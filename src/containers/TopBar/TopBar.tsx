b85dae403abe4a3260ef3fd1335c3f104a138404
Avner <avner.conceicao@hotmail.com>
2020-07-25 18:53:53 -0300

    refactor: add style in TopBar

--- src/containers/TopBar/TopBar.tsx
+++ src/containers/TopBar/TopBar.tsx
@@ -4,9 +4,17 @@ import { Row, Col } from 'react-bootstrap'
 import Logo from '../../components/Logo/Logo'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import CartButton from '../../components/CartButton/CartButton'
+
+const topBarStyle = {
+    paddingTop: '1.2em',
+    paddingBottom: '1.2em',
+    backgroundColor: '#4682B4',
+    marginBottom: '0.5em'
+}
+
 const TopBar = () => {
     return(
-        <Row>
+        <Row style={topBarStyle}>
             <Col md="2" xs={{ span: 4, order: 1 }}><Logo /></Col>
             <Col md={{ span: 7, offset: 1, order: 2}} sm={{ span: 7, offset: 1, order: 2 }} xs={{ span: 12, order: 3 }}><SearchBar /></Col>
             <Col md={{ span: 2, order: 3 }} xs={{ span: 4, order: 2 }}><CartButton /></Col>
