8e3587073e087e601efec6fe01065f637374fac5
Avner <avner.conceicao@hotmail.com>
2020-07-25 18:27:40 -0300

    feat: should change the value when a user types something

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,7 +2,11 @@ import * as React from 'react'
 import { InputGroup, Button, FormControl } from 'react-bootstrap'
 
 const SearchBar = () => {
-    
+    const initialState = {
+        textPokemon: ''
+    }
+	
+	const [state, setState] = React.useState(initialState)
     return (
         <InputGroup>
             <FormControl
@@ -10,7 +14,13 @@ const SearchBar = () => {
                 placeholder="Busque os Pokemon aqui"
                 aria-label="Busque os Pokemon aqui"
                 id="inputTextPokemon"
-                />
+                onChange={(e) => {
+                    e.preventDefault
+                    setState({
+                        textPokemon: e.target.value
+                    })
+                }}
+                value={state.textPokemon} />
             <InputGroup.Append>
                 <Button>Click me</Button>
             </InputGroup.Append>
