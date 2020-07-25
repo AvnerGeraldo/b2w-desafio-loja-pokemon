aa15229d99ae0698924d1cda717fc3ef06c3cf63
Avner <avner.conceicao@hotmail.com>
2020-07-25 20:49:48 -0300

    feat: should show a friendly error message when the data is not loaded

---
+++ src/containers/PokemonList/PokemonList.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+interface PokemonListProps {
+    pokemonData: Array<any>
+}
+
+export default class PokemonList extends React.Component<PokemonListProps, any> {
+    render() {
+        const { pokemonData } = this.props
+        
+        return (
+            <Container fluid className="h-100">
+                {(pokemonData && pokemonData.length === 0) && (
+                    <Row className="align-items-center">
+                        <Col className="text-center error-message-empty-content">
+                            <h3>Não há dados para serem exibidos.</h3><br /> 
+                            <p><i style={{
+                            fontSize: '4em'
+                        }}>:(</i></p></Col>
+                    </Row>
+                )}
+            </Container>
+        )
+    }
+}
