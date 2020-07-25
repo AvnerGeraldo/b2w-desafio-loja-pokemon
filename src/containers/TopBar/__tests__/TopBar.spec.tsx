9f9c17ab622dcdb2e8bfa6fe0ff6179113c9e8b9
Avner <avner.conceicao@hotmail.com>
2020-07-25 17:55:24 -0300

    test: should have a one search bar component inside the container

--- src/containers/TopBar/__tests__/TopBar.spec.tsx
+++ src/containers/TopBar/__tests__/TopBar.spec.tsx
@@ -3,10 +3,16 @@ import { shallow } from 'enzyme'
 
 import TopBar from '../TopBar'
 import Logo from '../../../components/Logo/Logo'
+import SearchBar from '../../../components/SearchBar/SearchBar';
 
 describe('TopBar Container', () => {
     it('should have a one logo component inside the container', () => {
         const wrapper = shallow(<TopBar />)
         expect(wrapper.find(Logo)).toHaveLength(1)
+    });
+
+    it('should have a one search bar component inside the container', () => {
+        const wrapper = shallow(<TopBar />)
+        expect(wrapper.find(SearchBar)).toHaveLength(1)
     });
 });
