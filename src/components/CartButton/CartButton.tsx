f4618bdce9ccd3063251eeeaf62f3a63c716c360
Avner <avner.conceicao@hotmail.com>
2020-07-27 05:05:02 -0300

    feat: count badges from cart list

--- src/components/CartButton/CartButton.tsx
+++ src/components/CartButton/CartButton.tsx
@@ -7,28 +7,33 @@ import { StoreState } from '../../store/types/storeState'
 import { IStateCart } from '../../store/types/cartTypes'
 
 import { Button, Badge } from 'react-bootstrap'
+import { PokemonData } from '../../store/types/pokemonDataTypes'
 
 interface CartButtonProps {
     isOpen: boolean
+    cartList: Array<PokemonData>
     openClose: (status: boolean) => void
 }
 
 const CartButton = (props: CartButtonProps) => {
+    
     return (
         <Button style={{
             display: 'block',
-            transition: '0.3s'
+            transition: '0.3s',
+            minWidth: '75px'
         }}
             variant="outline-dark"
             onClick={() => props.openClose(!props.isOpen)}>
-            Cart <Badge variant="light">1</Badge>
+            Cart <Badge variant="light">{props.cartList.length || ''}</Badge>
             <span className="sr-only">cart items</span>
         </Button>
     )
 }
 
-const mapStateToProps = ({ cartStore: { isOpen } }: StoreState): IStateCart => ({
-    isOpen
+const mapStateToProps = ({ cartStore: { isOpen, cartList } }: StoreState): IStateCart => ({
+    isOpen,
+    cartList
 })
 
 const mapDispatchToProps = (dispatch: Dispatch<actions.CartActions>) => ({
