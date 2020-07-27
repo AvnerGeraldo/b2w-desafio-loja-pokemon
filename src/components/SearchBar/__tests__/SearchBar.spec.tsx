d36f9d55c6f01fccaf27e448b5c3643e04c2f273
Avner <avner.conceicao@hotmail.com>
2020-07-27 20:14:32 -0300

    fix: adjust SearchBar spec for new props

--- src/components/SearchBar/__tests__/SearchBar.spec.tsx
+++ src/components/SearchBar/__tests__/SearchBar.spec.tsx
@@ -1,11 +1,22 @@
+import 'jsdom-global/register'
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import SearchBar from '../SearchBar';
+import { Provider } from 'react-redux';
+import store from '../../../store';
+
+const loadLocalStorage = (offset: number, limit: number) => ({})
+const setPage = (newPage: number) => ({ })
 
 describe('SearchBar Component', () => {
     it('should change the value when a user types something', () => {
-        const wrapper = shallow(<SearchBar />)
-        wrapper.find('#inputTextPokemon').simulate('change', { target: { value: 'some text' }})
-        expect(wrapper.find('#inputTextPokemon').props().value).toEqual('some text')
+        const wrapper = mount(
+            <Provider store={store}>
+                <SearchBar loadLocalStorage={loadLocalStorage} setPage={setPage} />
+            </Provider>
+        )
+        
+        wrapper.find('#inputTextPokemon').at(0).simulate('change', { target: { value: 'some text' }})
+        expect(wrapper.find('#inputTextPokemon').at(0).props().value).toEqual('some text')
     });
 });
