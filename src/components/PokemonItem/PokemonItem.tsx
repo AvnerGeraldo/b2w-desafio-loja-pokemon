946b2c38d1b244ce20f97e036690d4d9185f9179
Avner <avner.conceicao@hotmail.com>
2020-07-27 04:14:29 -0300

    feat: add pokemon data to cart

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import { Button, Col, Container } from 'react-bootstrap'
 
-export type PokemonItemProps = {
+import { PokemonData } from '../../store/types/pokemonDataTypes'
+import { StoreState } from '../../store/types/storeState'
+import { Dispatch } from 'redux'
+import * as actions from '../../store/actions'
+import { connect } from 'react-redux'
+
+interface IPokemonItem extends PokemonData {
     id: number
     name: string
     price: number
     image: string
+    addPokemonToCart?: (pokemon: PokemonData) => void
 }
 
 const imageStyle = {
@@ -13,16 +20,23 @@ const imageStyle = {
     width: '100%'
 }
 
-const PokemonItem = (props: PokemonItemProps) => {
-    const { id, name, price, image } = props
+const PokemonItem = (props: IPokemonItem) => {
+    const { id, name, price, image, addPokemonToCart } = props
     return (
         <Container className="pokemon-list-item">
             <Col><span className="d-none pokemon-id">{id}</span><img src={image} style={imageStyle} /></Col>
             <Col className="text-center text-capitalize pokemon-name"><span>{name}</span></Col>
             <Col className="text-center mt-1 mb-2 pokemon-price">R$ <span>{price.toLocaleString('pt-br')}</span></Col>
-            <Col className="text-center"><Button>Adicionar <i>Icon cart</i></Button></Col>
+            <Col className="text-center">
+                <Button onClick={() => addPokemonToCart({ id, name, price, image })}>Adicionar <i>Icon cart</i></Button>
+            </Col>
         </Container>
     )
 }
 
-export default PokemonItem
+const mapStateToProps = (state: StoreState) => state
+const mapDispatchToProps = (dispatch: Dispatch<actions.UpdateCart>) => ({
+    addPokemonToCart: (pokemon: PokemonData) => dispatch(actions.updateCart(pokemon))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonItem)
