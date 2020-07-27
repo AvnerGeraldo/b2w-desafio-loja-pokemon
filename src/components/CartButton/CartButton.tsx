8c25bbf1453c6babb664970960ae31ac8d45db9c
Avner <avner.conceicao@hotmail.com>
2020-07-27 03:06:35 -0300

    feat: connect CartButton to redux store

--- src/components/CartButton/CartButton.tsx
+++ src/components/CartButton/CartButton.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+
+import * as actions from '../../store/actions'
+import { StoreState } from '../../store/types/storeState'
+import { IStateCart } from '../../store/types/cartTypes'
+
 import { Button, Badge } from 'react-bootstrap'
 
-const CartButton = () => {
+interface CartButtonProps {
+    isOpen: boolean
+    openClose: any
+}
+
+const CartButton = (props: CartButtonProps) => {
     return (
-        <Button variant="outline-dark">
+        <Button style={{
+            display: 'block',
+            transition: '0.3s'
+        }}
+            variant="outline-dark"
+            onClick={() => props.openClose(!props.isOpen)}>
             Cart <Badge variant="light">1</Badge>
             <span className="sr-only">cart items</span>
         </Button>
     )
 }
 
-export default CartButton
+const mapStateToProps = ({ cartStore: { isOpen } }: StoreState): IStateCart => ({
+    isOpen
+})
+
+const mapDispatchToProps = (dispatch: Dispatch<actions.CartActions>) => ({
+    openClose: (isOpen: boolean) => dispatch(actions.openCloseCart(isOpen))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartButton)
