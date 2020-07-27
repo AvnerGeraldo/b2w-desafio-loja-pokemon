72f2a0f4180b6be850a5d0e5328cb3304e8943e4
Avner <avner.conceicao@hotmail.com>
2020-07-27 05:07:19 -0300

    feat: show cart items after a item is added

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -12,7 +12,9 @@ interface IPokemonItem extends PokemonData {
     name: string
     price: number
     image: string
+    isOpen?: boolean
     addPokemonToCart?: (pokemon: PokemonData) => void
+    openCloseCart?: (status: boolean) => void
 }
 
 const imageStyle = {
@@ -20,23 +22,40 @@ const imageStyle = {
     width: '100%'
 }
 
+const addToCart = (props: IPokemonItem) => {
+    const { id, name, price, image, addPokemonToCart, openCloseCart, isOpen } = props
+
+    addPokemonToCart({ id, name, price, image })
+
+    if (!isOpen) {
+        openCloseCart(!isOpen)
+    }
+
+    setTimeout(() => {
+        openCloseCart(false)
+    }, 3000)
+}
+
 const PokemonItem = (props: IPokemonItem) => {
-    const { id, name, price, image, addPokemonToCart } = props
+    const { id, name, price, image } = props
     return (
         <Container className="pokemon-list-item">
             <Col><span className="d-none pokemon-id">{id}</span><img src={image} style={imageStyle} /></Col>
             <Col className="text-center text-capitalize pokemon-name"><span>{name}</span></Col>
             <Col className="text-center mt-1 mb-2 pokemon-price">R$ <span>{price.toLocaleString('pt-br')}</span></Col>
             <Col className="text-center">
-                <Button onClick={() => addPokemonToCart({ id, name, price, image })}>Adicionar <i>Icon cart</i></Button>
+                <Button onClick={() => addToCart(props)}>Adicionar <i>Icon cart</i></Button>
             </Col>
         </Container>
     )
 }
 
-const mapStateToProps = (state: StoreState) => state
-const mapDispatchToProps = (dispatch: Dispatch<actions.UpdateCart>) => ({
-    addPokemonToCart: (pokemon: PokemonData) => dispatch(actions.updateCart(pokemon))
+const mapStateToProps = ({ cartStore: { isOpen } }: StoreState) => ({
+    isOpen
+})
+const mapDispatchToProps = (dispatch: Dispatch<actions.UpdateCart | actions.OpenCloseCart>) => ({
+    addPokemonToCart: (pokemon: PokemonData) => dispatch(actions.updateCart(pokemon)),
+    openCloseCart: (status: boolean) => dispatch(actions.openCloseCart(status))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PokemonItem)
