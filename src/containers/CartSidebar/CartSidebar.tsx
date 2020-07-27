78a35b521cb0a3b9e923afa5203b3c79b08e6f49
Avner <avner.conceicao@hotmail.com>
2020-07-27 05:06:06 -0300

    feat: show cart items

--- src/containers/CartSidebar/CartSidebar.tsx
+++ src/containers/CartSidebar/CartSidebar.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, Row, Button, Container } from 'react-bootstrap'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { StoreState } from '../../store/types/storeState'
+import { PokemonData } from '../../store/types/pokemonDataTypes'
 
+interface ICartSidebar {
+    isOpen: boolean
+    cartList?: Array<PokemonData>
+}
 
-class CartSidebar extends React.Component<any, any> {
+type CartSidebarType = {
+    isOpen: boolean
+    cartList?: Array<PokemonData>
+}
+
+class CartSidebar extends React.Component<CartSidebarType, any> {
+    constructor(props: ICartSidebar) {
+        super(props)
+    }
+
     render() {
-        const isOpen = true
+        const { isOpen = false, cartList } = this.props
+        let total = 0
 
         const SideBarContainer = styled.div`
-            min-height: 100vh;
+            min-height: 60vh;
             margin-right: ${isOpen ? '0px' : '-250px'};
             position: absolute;
             right: 0;
@@ -21,12 +38,44 @@ class CartSidebar extends React.Component<any, any> {
 
         return (
             <SideBarContainer className="col-lg-5 col-md-5 col-sm-5 col-xs-12 col-12">
-                <Col>
-                    Cart Sidebar
-                </Col>
+                <Row>
+                    <Col className="text-center sb-2 mb-2 lb-2"><h3>Carrinho</h3></Col>
+                </Row>
+                <Container style={{
+                    minHeight: '30vh'
+                }}>
+                    {(cartList && cartList.length > 0) && (
+                        cartList.map((item: PokemonData) => {
+                            total += item.price
+                            return (
+                                <Row key={item.id} style={{
+                                    borderBottom: "1px solid"
+                                }}>
+                                    <Col lg={2} md={3} sm={3} xs={3}><img src={item.image} width={60} height={60} /></Col>
+                                    <Col className="align-self-center">{item.name}</Col>
+                                    <Col lg={2} md={3} sm={3} xs={3} className="align-self-center">R$ <span>{item.price.toLocaleString('pt-br')}</span></Col>
+                                </Row>
+                            )
+                        })
+                    )}
+                </Container>
+                <Row>
+                    <Col lg={8} md={7} sm={7} xs={8}><h4>Total</h4></Col>
+                    <Col lg={4} md={5} sm={5} xs={4} className="text-right">R$ <span>{total.toLocaleString('pt-br')}</span></Col>
+                </Row>
+                <Row style={{ marginTop: '40px', marginBottom: "15px"}}>
+                    <Col>
+                        <Button className="btn btn-primary btn-lg btn-block">Finalizar</Button>
+                    </Col>
+                </Row>
             </SideBarContainer>
         )
     }
 }
 
-export default CartSidebar
+const mapStateToProps = ({ cartStore: { isOpen, cartList } }: StoreState) => ({
+    isOpen,
+    cartList
+})
+
+export default connect(mapStateToProps)(CartSidebar)
